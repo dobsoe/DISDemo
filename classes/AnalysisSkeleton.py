11cd0fd84abf4f5ad715a7a4280af7eaae8b894f
Ellie Dobson <edobson@pivotal.io>
2016-03-08 09:28:38 +0000

    new code

--- classes/AnalysisSkeleton.py
+++ classes/AnalysisSkeleton.py
@@ -4,9 +4,21 @@ import pandas as pd
 
 class analysisSkeleton:
 
-    def __init__(self, jsondata):
-        print jsondata
-        self.data=jsondata
+    def __init__(self, data):
+        self.data=data
 
     def getAttribute(self):
-        return str(self.data)
+        return self.data
+
+    def cleanData(self):
+        s=self.data['lineStatuses']
+        self.data['statusSeverity']=s.iloc[:].apply(lambda x: x[0]['statusSeverity'])
+        self.data['statusSeverityDescription']=s.iloc[:].apply(lambda x: x[0]['statusSeverityDescription'])
+        self.data['validityPeriods']=s.iloc[:].apply(lambda x: x[0]['validityPeriods'])
+        self.data['created2']=s.iloc[:].apply(lambda x: x[0]['created'])
+        self.data['line']=s.iloc[:].apply(lambda x: x[0]['id'])
+        del data['name']
+        del data['id']
+        #del data['lineStatuses']
+        #del data['routeSections']
+        return data
--- classes/S3Connection.py
+++ classes/S3Connection.py
@@ -8,14 +8,37 @@ class s3Connection:
         self.access_key = 'AKIAJYA6PMAUGLR37S4Q'
         self.secret_key = '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'
         self.bucket='pivotal-london-dis'
-        self.key='tfl_api_line_mode_status_tube_2015-02-24_11:51:45.json'
+        self.testkey='tfl_api_line_mode_status_tube_2015-02-24_11:51:45.json'
 
-    def getjson(self):
+    def getjson_test(self):
         conn = boto.s3.connect_to_region('eu-west-1',
                                           aws_access_key_id = self.access_key,
                                           aws_secret_access_key = self.secret_key,
                                           calling_format = boto.s3.connection.OrdinaryCallingFormat(),)
         bucket_dis=conn.get_bucket(self.bucket)
-        key=bucket_dis.get_key(self.key)
+        key=bucket_dis.get_key(self.testkey)
         data = key.get_contents_as_string()
+        return data
+
+    def getjson(self, maxfiles=-1):
+        conn = boto.s3.connect_to_region('eu-west-1',
+                                          aws_access_key_id = self.access_key,
+                                          aws_secret_access_key = self.secret_key,
+                                          calling_format = boto.s3.connection.OrdinaryCallingFormat(),)
+        bucket_dis=conn.get_bucket(self.bucket)
+
+        i=0
+        for key in bucket_dis.list():
+            i=i+1
+            if(i%100==0):
+                print (i+'files read')
+            if((maxfiles>0) and (i>maxfiles)):
+                break
+            key=bucket_dis.get_key(key.name.encode('utf-8'))
+            df=pd.read_json(key.get_contents_as_string())
+            if i==1:
+                data=df
+            else:
+                data=pd.concat([data,df])
+        print 'done'
         return data
